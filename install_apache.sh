3584959a590bb3be61c27e7b39ec7689e674344b
devops-aws-geek <128142357+devops-aws-geek@users.noreply.github.com>
2023-03-24 13:36:05 +0530

    installing webapp using user data

--- install_apache.sh
+++ install_apache.sh
@@ -5,3 +5,6 @@ yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
 echo "Hello World from $(hostname -f)" > /var/www/html/index.html
+yum install -y docker
+systemctl start docker
+docker run --name webapp -d -it -p 8080:8080 deepak2717/devopswithdeepak-docker-demo:devopswithdeepak-docker-demo
