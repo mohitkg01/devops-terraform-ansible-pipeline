33e5ac120bb1dc781f4482b2f904cd3851400546
Mohit Kumar Gond <109984401+mohitkg01@users.noreply.github.com>
2024-03-05 23:43:30 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@
 
 terraform {
   backend "s3" {
-    bucket = "devops-terrafom-backend01"
+    bucket = "devops-terrafom-backend-mohit"
     key    = "project/remote.tfstate"
     region = "us-east-1"
   }
